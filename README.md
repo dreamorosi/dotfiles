1f4a63fcf549eb8d445fa9bc34da28969b4d1454
Andrea Amorosi <dreamorosi@gmail.com>
2023-09-25 13:50:04 +0000

    :sparkles: add debug + symlink scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dotfiles
+
+This repo contains some of the shared dotfiles that I use across untrusted hosts.
--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,37 @@ curl -fsSL https://fnm.vercel.app/install | zsh
 # Install pure prompt
 mkdir -p "$HOME/.zsh"
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+# Symlink files
+current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+dotfiles_source="${current_dir}"
+deny_list=("debug.sh" "bootstrap.sh" ".git/")
+
+while read -r file; do
+    # Check if the file is in the deny list
+    skip_file=false
+    for item in "${deny_list[@]}"; do
+        if [[ -d "${item}" && "${file}" == "${dotfiles_source}/${item}/"* ]] || [[ "${file}" == "${dotfiles_source}/${item}"* ]]; then
+            printf 'Skipping file %s\n' "${file}"
+            skip_file=true
+            break
+        fi
+    done
+
+    if [ "${skip_file}" = true ]; then
+        continue
+    fi
+
+    # If not, we symlink it
+    relative_file_path="${file#"${dotfiles_source}"/}"
+    target_file="${HOME}/${relative_file_path}"
+    target_dir="${target_file%/*}"
+
+    if test ! -d "${target_dir}"; then
+        mkdir -p "${target_dir}"
+    fi
+
+    printf 'Installing dotfiles symlink %s\n' "${target_file}"
+    ln -sf "${file}" "${target_file}"
+
+done < <(find "${dotfiles_source}" -type f)
